44ffdddde42b5d84afda9847fdf6702164ace8f0
Ankit Keshari <ankitkeshari719@gmail.com>
2024-12-15 13:00:34 +0530

    Added Theme

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { purple, amber } from '@mui/material/colors';
+import CssBaseline from "@mui/material/CssBaseline";
+
 import { Checkout, Product, Products, Home } from "./pages";
+
+const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
+  palette: {
+    primary: {
+      main: purple[900],
+    },
+    secondary: {
+      main: amber[900],
+    },
+    // primary: {
+    //   light: '#757ce8',
+    //   main: '#3f50b5',
+    //   dark: '#002884',
+    //   contrastText: '#fff',
+    // },
+    // secondary: {
+    //   light: '#ff7961',
+    //   main: '#f44336',
+    //   dark: '#ba000d',
+    //   contrastText: '#000',
+    // },
+  },
+});
 
 function App() {
   return (
     <div className="App">
       <Router basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route exact path="/checkout" element={<Checkout />} />
-          <Route path="/:category/:productId" element={<Product />} />
-          <Route exact path="/:category" element={<Products />} />
-          <Route exact path="/" element={<Home />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route exact path="/checkout" element={<Checkout />} />
+            <Route path="/:category/:productId" element={<Product />} />
+            <Route exact path="/:category" element={<Products />} />
+            <Route exact path="/" element={<Home />} />
+          </Routes>
+        </ThemeProvider>
       </Router>
     </div>
   );
