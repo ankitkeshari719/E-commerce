bbda2a7e5178ec24b82ef73f0e0b843f9a993f95
Ankit Keshari <ankitkeshari719@gmail.com>
2024-12-15 12:34:25 +0530

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Checkout, Product, Products, Home } from "./pages";
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Router basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route exact path="/checkout" element={<Checkout />} />
+          <Route path="/:category/:productId" element={<Product />} />
+          <Route exact path="/:category" element={<Products />} />
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Product } from "./Product";
+export { default as Products } from "./Products";
+export { default as Checkout } from "./Checkout";
+export { default as Home } from "./Home";
