9b4d94d903a4d38e63e636ede3b1c40d866080ab
Ankit Keshari <ankitkeshari719@gmail.com>
2024-12-24 21:38:15 +0530

    Added items Dynamically

--- src/components/productSuggestions.js
+++ src/components/productSuggestions.js
@@ -4,10 +4,10 @@ import GridItem from "./gridItem";
 
 export default function ProductSuggestions({ products }) {
   return (
-    <Grid container spacing={2}>
+    <Grid container spacing={1}>
       {products.map((product) => (
-        <Grid key={product.id} item xs={12} sm={6} md={3}>
-          <GridItem />
+        <Grid key={product.id} item xs={12} sm={6} md={4}>
+          <GridItem product={product} />
         </Grid>
       ))}
     </Grid>
